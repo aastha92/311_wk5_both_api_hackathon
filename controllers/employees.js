a0a53079d07ad3f7b9739bddcbfbd5f729ab0b15
aastha.92 <npathak@gmail.com>
2019-12-12 21:09:17 -0600

    updated controllers

--- controllers/employees.js
+++ controllers/employees.js
@@ -1,6 +1,32 @@
-const getEmployees = (req,res) =>{}
+const pool = require('./sql/connection');
 
-const getEmployeesById = (req,res) =>{}
+const getEmployees = (req,res) =>{
+    let sql = 'SELECT * FROM ?? LIMIT ?';
+    let replacement = ["employees", "50"];
+    sql = mysql.format(sql, replacements)
+
+    pool.query(sql, (err, rows) => {
+        if (err) {
+          console.log({ 'message': 'Error occurred: ' + err })
+          return res.status(500).send('An unexpected error occured')
+        }
+        res.json(rows)
+      });
+}
+
+const getEmployeesById = (req,res) =>{
+    let sql = 'SELECT * FROM ?? WHERE ?? = ?';
+    let replacement = ["employees", "emp_no", "req.id.params"];
+    sql = mysql.format(sql, replacements)
+
+    pool.query(sql, (err, rows) => {
+        if (err) {
+          console.log({ 'message': 'Error occurred: ' + err })
+          return res.status(500).send('An unexpected error occured')
+        }
+        res.json(rows)
+      });
+}
 
 
 const getEmployeesByFirstName = (req,res) => {}
