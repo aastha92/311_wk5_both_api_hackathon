0609dc70cf49859fe9b1628eef51e73ec1cf5788
aastha.92 <npathak@gmail.com>
2019-12-12 19:49:55 -0600

    controllers

---
+++ controllers/employees.js
@@ -0,0 +1,9 @@
+const getEmployees = (req,res) =>{}
+
+const getEmployeesById = (req,res) =>{}
+
+
+const getEmployeesByFirstName = (req,res) => {}
+
+module.exports = { getEmployees, getEmployeesById, getEmployeesByFirstName }
+
